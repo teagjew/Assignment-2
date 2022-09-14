59ef5928baea2d59a845b7c1e8993015fb874b3d
94arodriguez <aarodriguez10@buffs.wtamu.edu>
2022-09-14 18:37:19 -0500

    Alec's changes

---
+++ DeliveryCart/Pages/Delivery Person/Decline.cshtml.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Assignment_2.Models;
+
+namespace Assignment_2.Pages.DeliveryPerson
+{
+    public class DeclineModel : PageModel
+    {
+        private readonly Assignment_2.Models.DatabaseContext _context;
+
+        public DeclineModel(Assignment_2.Models.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Order Order { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order.FirstOrDefaultAsync(m => m.OrderID == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Order = order;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+            var order = await _context.Order.FindAsync(id);
+
+            if (order != null)
+            {
+                Order = order;
+                _context.Order.Remove(Order);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ DeliveryCart/Pages/Delivery Person/Edit.cshtml.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Assignment_2.Models;
+
+namespace Assignment_2.Pages.DeliveryPerson
+{
+    public class EditModel : PageModel
+    {
+        private readonly Assignment_2.Models.DatabaseContext _context;
+
+        public EditModel(Assignment_2.Models.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Order Order { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Order = await _context.Order.FirstOrDefaultAsync(p => p.OrderID == id);
+
+            if (Order == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(Order.OrderID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Order.Any(e => e.OrderID == id);
+        }
+    }
+}
