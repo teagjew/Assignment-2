03eb192cdb9f0d7c8403a0e9af10029ecf45fff6
Teagan Jewett <tajewett1@buffs.wtamu.edu>
2022-10-04 21:30:00 -0500

    First Test Commit

---
+++ DeliveryCart.Tests/UnitTest1.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Assignment_2.Pages;
+using Assignment_2.Models;
+
+namespace DeliveryCart.Tests;
+
+    public class UnitTest1
+    {
+        // [Theory]
+        // [InlineData(DatabaseContext.Item.Where(i => i.ItemID = 1), DatabaseContext.Item.Where(i => i.ItemID = 2))]
+        // public void Test1(double value1, double value2)
+        // {
+        
+        // }
+        
+        [Fact]
+        public async Task CreateOrder_OrderIsAdded()
+        {
+            using (var db = new DatabaseContext(Utilities.TestDbContextOptions()))
+            {
+                // Arrange
+                var id = 1;
+                string name = "John Doe";
+                string address = "123 Main St";
+                double total = 1.25;
+
+                List<Order> Orders = new List<Order>{
+                    new Order
+                    {
+                        CustomerName = name,
+                        CustomerAddress = address,
+                        OrderTotal = total,
+                        Status = "Pending"
+                    }
+                };
+
+                List<Item> Items = new List<Item>{
+                    new Item{Name = "Milk", Price = 2.99, Status = "In Cart"},
+                    new Item{Name = "Eggs", Price = 3.99, Status = "In Cart" }
+                };
+
+                List<OrderedItem> orderedItems = new List<OrderedItem>{
+                new OrderedItem { Item = Items[0], Order = Orders[0] },
+                new OrderedItem { Item = Items[1], Order = Orders[0] }
+                };
+
+                var expectedOrder = new Order() { OrderID = id, CustomerName = name, CustomerAddress = address, OrderTotal = total, Status = "Pending", OrderedItems = orderedItems };
+
+                // Act
+                db.Item.AddRange(Items);
+                await db.CreateOrder(name, address, total);
+                //db.OrderedItems.AddRange(orderedItems);
+
+                // Assert
+                var actualOrder = await db.FindAsync<Order>(id);
+                Assert.Equal(expectedOrder, actualOrder);
+            }
+        }
+    }
