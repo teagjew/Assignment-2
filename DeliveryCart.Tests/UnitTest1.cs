322edc6981e849d6904f4cb131cd9c86f8ce1a55
Teagan Jewett <tajewett1@buffs.wtamu.edu>
2022-10-05 21:34:54 -0500

    Fifth Testing Commit

--- DeliveryCart.Tests/UnitTest1.cs
+++ DeliveryCart.Tests/UnitTest1.cs
@@ -24,23 +24,137 @@ namespace DeliveryCart.Tests;
             using (var db = new DatabaseContext(Utilities.TestDbContextOptions()))
             {
                 List<Item> seedItems = DatabaseContext.GetItemsSeed();
-                //List<Order> seedOrders = DatabaseContext.GetOrdersSeed();
-                //List<OrderedItem> seedOrderedItems = DatabaseContext.GetOrderedItemsSeed(seedItems, seedOrders);
                 await db.AddRangeAsync(seedItems);
-                //await db.AddRangeAsync(seedOrders);
-                //await db.AddRangeAsync(seedOrderedItems);
                 await db.SaveChangesAsync();
 
-                Item expectedItem = new Item {ItemID = 1, Name = "Milk", Price = 2.99, Status = "Not in Cart"};
+                Item expectedItem = new Item {ItemID = 1, Name = "Milk", Price = 2.99, Status = "In Cart"};
 
+                db.ChangeTracker.Clear();
                 await db.AddToCart(1);
 
                 Item actualItem = db.Item.Where(i => i.ItemID == 1).AsNoTracking().FirstOrDefault();
-                Assert.Equal(expectedItem, actualItem);
-                //Assert.Equal(1,1);
+                Assert.Equal(expectedItem.Status, actualItem.Status);
             }
         }
         
+        [Fact]
+        public async Task ClearCart_CartIsCleared()
+        {
+            using (var db = new DatabaseContext(Utilities.TestDbContextOptions()))
+            {
+                List<Item> seedItems = DatabaseContext.GetItemsSeed();
+                await db.AddRangeAsync(seedItems);
+                await db.SaveChangesAsync(); 
+                db.ChangeTracker.Clear();
+                await db.AddToCart(1);
+
+                db.ChangeTracker.Clear();
+                await db.ClearCart();
+
+                List<Item> actualItem = db.Item.Where(i => i.Status == "In Cart").ToList();
+                Assert.Equal(0, actualItem.Count());
+            }
+        }
+
+        [Fact]
+        public async Task RemoveCartItem_ItemIsRemoved()
+        {
+            using (var db = new DatabaseContext(Utilities.TestDbContextOptions()))
+            {
+                List<Item> seedItems = DatabaseContext.GetItemsSeed();
+                await db.AddRangeAsync(seedItems);
+                await db.SaveChangesAsync(); 
+                db.ChangeTracker.Clear();
+                await db.AddToCart(1);
+
+                Item expectedItem = new Item {ItemID = 1, Name = "Milk", Price = 2.99, Status = "Not in Cart"};
+
+                db.ChangeTracker.Clear();
+                await db.RemoveCartItem(1);
+
+                Item actualItem = db.Item.Where(i => i.ItemID == 1).AsNoTracking().FirstOrDefault();
+                Assert.Equal(expectedItem.Status, actualItem.Status);
+            }
+        }
+
+        [Fact]
+        public async Task AcceptOrder_OrderAccepted()
+        {
+            using (var db = new DatabaseContext(Utilities.TestDbContextOptions()))
+            {
+                List<Order> seedOrders = DatabaseContext.GetOrdersSeed();
+                await db.AddRangeAsync(seedOrders);
+                await db.SaveChangesAsync(); 
+
+                Order expectedOrder = new Order { CustomerName = "Jane Smith", CustomerAddress = "123 S 5th St, Lubbock, TX 79540", OrderTotal = 6.98, Status = "Accepted"};
+
+                db.ChangeTracker.Clear();
+                await db.AcceptOrder(1);
+
+                Order actualOrder = db.Order.Where(i => i.OrderID == 1).AsNoTracking().FirstOrDefault();
+                Assert.Equal(expectedOrder.Status, actualOrder.Status);
+            }
+        }
+
+        [Fact]
+        public async Task DeclineOrder_OrderDeclined()
+        {
+            using (var db = new DatabaseContext(Utilities.TestDbContextOptions()))
+            {
+                List<Order> seedOrders = DatabaseContext.GetOrdersSeed();
+                await db.AddRangeAsync(seedOrders);
+                await db.SaveChangesAsync(); 
+
+                Order expectedOrder = new Order { CustomerName = "Jane Smith", CustomerAddress = "123 S 5th St, Lubbock, TX 79540", OrderTotal = 6.98, Status = "Declined"};
+
+                db.ChangeTracker.Clear();
+                await db.DeclineOrder(1);
+
+                Order actualOrder = db.Order.Where(i => i.OrderID == 1).AsNoTracking().FirstOrDefault();
+                Assert.Equal(expectedOrder.Status, actualOrder.Status);
+            }
+        }
+
+        [Fact]
+        public async Task UpdateStatus_StatusUpdated()
+        {
+            using (var db = new DatabaseContext(Utilities.TestDbContextOptions()))
+            {
+                List<Order> seedOrders = DatabaseContext.GetOrdersSeed();
+                await db.AddRangeAsync(seedOrders);
+                await db.SaveChangesAsync(); 
+
+                Order expectedOrder = new Order { CustomerName = "Jane Smith", CustomerAddress = "123 S 5th St, Lubbock, TX 79540", OrderTotal = 6.98, Status = "In Progress"};
+
+                db.ChangeTracker.Clear();
+                await db.UpdateStatus(1, "In Progress");
+
+                Order actualOrder = db.Order.Where(i => i.OrderID == 1).AsNoTracking().FirstOrDefault();
+                Assert.Equal(expectedOrder.Status, actualOrder.Status);
+            }
+        }
+
+        [Fact]
+        public async Task CartTotal_ReturnsTotal()
+        {
+            using (var db = new DatabaseContext(Utilities.TestDbContextOptions()))
+            {
+                List<Item> seedItems = DatabaseContext.GetItemsSeed();
+                await db.AddRangeAsync(seedItems);
+                await db.SaveChangesAsync(); 
+                db.ChangeTracker.Clear();
+                await db.AddToCart(1);
+                db.ChangeTracker.Clear();
+                await db.AddToCart(2);
+                db.ChangeTracker.Clear();
+
+                double expectedTotal = 6.98;
+
+                double actualTotal = db.CartTotal();
+                Assert.Equal(expectedTotal, actualTotal); 
+            }
+        }
+
         [Fact]
         public async Task CreateOrder_OrderIsCreated()
         {
